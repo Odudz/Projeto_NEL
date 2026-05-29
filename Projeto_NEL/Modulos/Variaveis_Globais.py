174bcfd7e93f1e12f0c3ecb90c77695e3af039b5
leonardoe10 <leonardopoliveira50@gmail.com>
2026-05-29 11:36:31 -0300

    Alteração variaveis
    Adicionado module variaveis global

---
+++ Projeto_NEL/Modulos/Variaveis_Globais.py
@@ -0,0 +1,16 @@
+import pygame as pg
+LARGURA = 1280
+ALTURA = 720
+
+tela = pg.display.set_mode((LARGURA, ALTURA))
+
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+VERMELHO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 100, 255)
+AMARELO = (255, 255, 0)
+CINZA = (40, 40, 40)
+
+IMAGENS = "Imagens"
+SONS    = "Sons"
